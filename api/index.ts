11ce64fb6306178b350b2d61479777b0a78c92d6
lekhai123 <k123456lhk@gmail.com>
2026-09-02 09:49:28 +0700

    huhuuhuhuhu

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 // FILE: api/index.ts
 // File này làm mồi nhử để Vercel nhận diện API, sau đó gọi thẳng vào bản đã nén
-import app from "../dist-api/index.js";
+import app from "../server";
 
 export default app;
